8bc49fc62f9f6f8e5284e1ae7fb3ea9506995895
JachyHm <jachym.hurtik@gmail.com>
2021-01-17 15:57:19 +0100

    Repaired admin request with no images included

--- admin/request.php
+++ admin/request.php
@@ -193,33 +193,34 @@ if (!empty($queryResult)) {
                         $sql->execute();
                         $queryResult = $sql->get_result();
                         if (!empty($queryResult)) {
-                            while ($file = $queryResult->fetch_assoc()["image_path"]) {
+                            while ($res = $queryResult->fetch_assoc()) {
+                                $file = $res["image_path"];
                                 echo "
-                        <div style=\"background-color:transparent;\">
-                            <div class=\"block-grid\" style=\"min-width: 320px; max-width: 500px; overflow-wrap: break-word; word-wrap: break-word; word-break: break-word; Margin: 0 auto; background-color: transparent;\">
-                                <div style=\"border-collapse: collapse;display: table;width: 100%;background-color:transparent;\">
-                                    <!--[if (mso)|(IE)]><table width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"background-color:transparent;\"><tr><td align=\"center\"><table cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"width:500px\"><tr class=\"layout-full-width\" style=\"background-color:transparent\"><![endif]-->
-                                    <!--[if (mso)|(IE)]><td align=\"center\" width=\"500\" style=\"background-color:transparent;width:500px; border-top: 0px solid transparent; border-left: 0px solid transparent; border-bottom: 0px solid transparent; border-right: 0px solid transparent;\" valign=\"top\"><table width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\"><tr><td style=\"padding-right: 0px; padding-left: 0px; padding-top:5px; padding-bottom:5px;\"><![endif]-->
-                                    <div class=\"col num12\" style=\"min-width: 320px; max-width: 500px; display: table-cell; vertical-align: top; width: 500px;\">
-                                        <div class=\"col_cont\" style=\"width:100% !important;\">
-                                            <!--[if (!mso)&(!IE)]><!-->
-                                            <div style=\"border-top:0px solid transparent; border-left:0px solid transparent; border-bottom:0px solid transparent; border-right:0px solid transparent; padding-top:5px; padding-bottom:5px; padding-right: 0px; padding-left: 0px;\">
-                                                <!--<![endif]-->
-                                                <div align=\"center\" class=\"img-container center autowidth\" style=\"padding-right: 0px;padding-left: 0px;\">
-                                                    <!--[if mso]><table width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\"><tr style=\"line-height:0px\"><td style=\"padding-right: 0px;padding-left: 0px;\" align=\"center\"><![endif]-->
-                                                    <img align=\"center\" alt=\"$file\" border=\"0\" class=\"center autowidth\" src=\"https://dls.rw.jachyhm.cz/files/requests_images/$file\" style=\"text-decoration: none; -ms-interpolation-mode: bicubic; height: auto; border: 0; width: 100%; max-width: 500px; display: block;\" title=\"$file\" width=\"500\"/>
-                                                    <!--[if mso]></td></tr></table><![endif]-->
-                                                </div>
-                                                <!--[if (!mso)&(!IE)]><!-->
-                                            </div>
-                                            <!--<![endif]-->
-                                        </div>
-                                    </div>
-                                    <!--[if (mso)|(IE)]></td></tr></table><![endif]-->
-                                    <!--[if (mso)|(IE)]></td></tr></table></td></tr></table><![endif]-->
-                                </div>
-                            </div>
-                        </div>";
+						<div style=\"background-color:transparent;\">
+							<div class=\"block-grid\" style=\"min-width: 320px; max-width: 500px; overflow-wrap: break-word; word-wrap: break-word; word-break: break-word; Margin: 0 auto; background-color: transparent;\">
+								<div style=\"border-collapse: collapse;display: table;width: 100%;background-color:transparent;\">
+									<!--[if (mso)|(IE)]><table width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"background-color:transparent;\"><tr><td align=\"center\"><table cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"width:500px\"><tr class=\"layout-full-width\" style=\"background-color:transparent\"><![endif]-->
+									<!--[if (mso)|(IE)]><td align=\"center\" width=\"500\" style=\"background-color:transparent;width:500px; border-top: 0px solid transparent; border-left: 0px solid transparent; border-bottom: 0px solid transparent; border-right: 0px solid transparent;\" valign=\"top\"><table width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\"><tr><td style=\"padding-right: 0px; padding-left: 0px; padding-top:5px; padding-bottom:5px;\"><![endif]-->
+									<div class=\"col num12\" style=\"min-width: 320px; max-width: 500px; display: table-cell; vertical-align: top; width: 500px;\">
+										<div class=\"col_cont\" style=\"width:100% !important;\">
+											<!--[if (!mso)&(!IE)]><!-->
+											<div style=\"border-top:0px solid transparent; border-left:0px solid transparent; border-bottom:0px solid transparent; border-right:0px solid transparent; padding-top:5px; padding-bottom:5px; padding-right: 0px; padding-left: 0px;\">
+												<!--<![endif]-->
+												<div align=\"center\" class=\"img-container center autowidth\" style=\"padding-right: 0px;padding-left: 0px;\">
+													<!--[if mso]><table width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\"><tr style=\"line-height:0px\"><td style=\"padding-right: 0px;padding-left: 0px;\" align=\"center\"><![endif]-->
+													<img align=\"center\" alt=\"$file\" border=\"0\" class=\"center autowidth\" src=\"https://dls.rw.jachyhm.cz/files/requests_images/$file\" style=\"text-decoration: none; -ms-interpolation-mode: bicubic; height: auto; border: 0; width: 100%; max-width: 500px; display: block;\" title=\"$file\" width=\"500\"/>
+													<!--[if mso]></td></tr></table><![endif]-->
+												</div>
+												<!--[if (!mso)&(!IE)]><!-->
+											</div>
+											<!--<![endif]-->
+										</div>
+									</div>
+									<!--[if (mso)|(IE)]></td></tr></table><![endif]-->
+									<!--[if (mso)|(IE)]></td></tr></table></td></tr></table><![endif]-->
+								</div>
+							</div>
+						</div>";
                             }
                         }
                         ?>
